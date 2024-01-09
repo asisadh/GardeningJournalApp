59348dae498d912d05c4f47921978f0998714498
Aashish Adhikari <asis.adh@gmail.com>
2024-01-09 18:49:44 -0500

    starting

---
+++ app/src/main/java/com/aashish/gardeningjournalapp/fragments/GardenLogFragment.kt
@@ -0,0 +1,24 @@
+package com.aashish.gardeningjournalapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aashish.gardeningjournalapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GardenLogFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GardenLogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_garden_log, container, false)
+    }
+}
